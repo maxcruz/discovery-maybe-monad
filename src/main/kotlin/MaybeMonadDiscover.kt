b1ac3192bbeb13c9d02dcc292557f5e4ffaaab48
Max Cruz <max.cruz@mytaxi.com>
2019-11-29 14:11:21 +0100

    The problem: create an evaluator for divisions

---
+++ src/main/kotlin/MaybeMonadDiscover.kt
@@ -0,0 +1,7 @@
+/*
+ Suppose that we want to create an evaluator for divisions
+ Math             Program
+ 1                Value 1
+ 6 / 2            Division Value 6 Value 2
+ 8 / (4 / 2)      Division (Value 8) (Division (Value 4) (Value 2))
+ */
