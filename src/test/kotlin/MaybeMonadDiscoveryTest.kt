c8ac0d99561866645faae3fe89a826595a7730bf
Max Cruz <max.cruz@mytaxi.com>
2019-11-30 18:11:43 +0100

    Update tests to expect the maybe type

--- src/test/kotlin/MaybeMonadDiscoveryTest.kt
+++ src/test/kotlin/MaybeMonadDiscoveryTest.kt
@@ -1,5 +1,5 @@
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
+import org.junit.Assert.assertSame
 import org.junit.Test
 
 
@@ -8,24 +8,24 @@ class MaybeMonadDiscoveryTest {
     @Test
     fun `GIVEN a expression with just a value WHEN the evaluator is called SHOULD return the value`() {
         val singleValue = evaluator(Exp.Val(1))
-        assertEquals(1, singleValue)
+        assertEquals(Maybe.Just(1), singleValue)
     }
 
     @Test
     fun `GIVEN a division WHEN the evaluator is called SHOULD evaluate the result`() {
         val singleDivision = evaluator(Exp.Div(Exp.Val(6), Exp.Val(2)))
-        assertEquals(3, singleDivision)
+        assertEquals(Maybe.Just(3), singleDivision)
     }
 
     @Test
     fun `GIVEN a composed expression WHEN the evaluator is called SHOULD resolve the nested operations`() {
         val composedExpression = evaluator(Exp.Div(Exp.Val(8), Exp.Div(Exp.Val(4), Exp.Val(2))))
-        assertEquals(4, composedExpression)
+        assertEquals(Maybe.Just(4), composedExpression)
     }
 
     @Test
     fun `GIVEN an undefined division WHEN the evaluator is called SHOULD return null`() {
         val undefinedDivision = evaluator(Exp.Div(Exp.Val(2), Exp.Val(0)))
-        assertNull(undefinedDivision)
+        assertSame(Maybe.Nothing, undefinedDivision)
     }
 }
